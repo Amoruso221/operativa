fc91af48b8f961d92057702ff46946f1a0447daf
Matias Amoruso <matias.amoruso@gmail.com>
2021-11-02 19:48:20 -0300

    Cambios en models

--- vacunatorios/models.py
+++ vacunatorios/models.py
@@ -7,11 +7,13 @@ from django.contrib.auth.models import User
 class Sede(models.Model):
     nombre = models.CharField(max_length=50)
     calle = models.CharField(max_length=50)
-    numero = models.IntegerField()
+    numero = models.IntegerField(default=0)
     vacuna = models.CharField(max_length=50)
+    canales = models.IntegerField(default=0)
+    espera_maxima = models.IntegerField(default=0)
+    longitud_cola = models.IntegerField(default=0)
     abierta = models.BooleanField(default=True)
-    canales = models.IntegerField()
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nombre
+        txt = "Nombre: {0}, Direccion: {1} {2}, Vacuna: {3}, Canales: {4}, Espera maxima (mins): {5}"
+        return txt.format(self.nombre, self.calle, self.numero, self.vacuna, self.canales, self.espera_maxima)

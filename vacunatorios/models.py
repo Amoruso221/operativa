b1de35a74748b7b0fd1725aed8f4fb97662b4926
Matias Amoruso <matias.amoruso@gmail.com>
2021-11-02 18:12:58 -0300

    funcion self.nombre en models

--- vacunatorios/models.py
+++ vacunatorios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,5 +11,7 @@ class Sede(models.Model):
     vacuna = models.CharField(max_length=50)
     abierta = models.BooleanField(default=True)
     canales = models.IntegerField()
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.nombre

a2fd575245ef207f6e46ae61aba945e7e1bd976d
Matias Amoruso <matias.amoruso@gmail.com>
2021-11-04 21:26:53 -0300

    html index

--- proyectovacunatorios/settings.py
+++ proyectovacunatorios/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proyectovacunatorios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'vacunatorios/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- proyectovacunatorios/urls.py
+++ proyectovacunatorios/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vacunatorios import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('sedes/', views.sedes, name='sedes'),
     path('admin/', admin.site.urls),
 ]
